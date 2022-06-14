0a19b25df792447db8235b866d95f7ae0bb1a099
moly li <molyli@lxr-4.local>
2022-06-14 22:49:34 +0200

    update

--- 03. Classification/3-3. Support Vector Machine (SVM)/SVM.R
+++ 03. Classification/3-3. Support Vector Machine (SVM)/SVM.R
@@ -1,5 +1,4 @@
-#K-Nearest Neighbors(K_NN)
-
+#support Vector Machine(SVM)
 
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
@@ -18,13 +17,18 @@ training_set[, 1:2] = scale(training_set[, 1:2])
 test_set[, 1:2] = scale(test_set[, 1:2])
 
 
-#Fitting KNN to Training set and Predicting the Test set results
-library(class)
-y_pred = knn(train = training_set[,-3],
-             test = test_set[, -3],
-             cl = training_set[, 3],
-             k =5)
+#Fitting Classifier to Training set
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear') 
+
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
 #y_pred
+
 
 #making the Confusion matrix
 cm = table(test_set[, 3],y_pred)
@@ -39,16 +43,13 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = knn(train = training_set[,-3],
-             test = grid_set,
-             cl = training_set[, 3],
-             k =5)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'K-NN(Training set)',
+     main = 'SVM (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen2', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualising the Test set results
@@ -58,12 +59,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = knn(train = training_set[,-3],
-             test = grid_set,
-             cl = training_set[, 3],
-             k =5)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'K-NN (Test set)',
+     main = 'SVM(Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)

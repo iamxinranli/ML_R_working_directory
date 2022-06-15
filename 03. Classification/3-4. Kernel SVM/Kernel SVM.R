178a745c29cbf5c72b6f29e543ef4315cc6c1bb0
moly li <molyli@lxr-4.local>
2022-06-15 11:55:41 +0200

    update

--- 03. Classification/3-4. Kernel SVM/Kernel SVM.R
+++ 03. Classification/3-4. Kernel SVM/Kernel SVM.R
@@ -1,4 +1,5 @@
-#Classification template
+#Kernel SVM
+
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[, 3:5]
@@ -15,13 +16,15 @@ test_set = subset(dataset, split == FALSE)
 training_set[, 1:2] = scale(training_set[, 1:2])
 test_set[, 1:2] = scale(test_set[, 1:2])
 
-
-#Fitting Classifier to Training set
-# creat the right classifier here
+#Fitting Kernal SVM to Training set
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel= 'radial')
 
 # Predicting the Test set results
 y_pred = predict(classifier, newdata = test_set[-3])
-
 
 
 #making the Confusion matrix
@@ -39,7 +42,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, type = 'response', newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Training set)',
+     main = 'kernel SVM (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -55,11 +58,10 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Test set)',
+     main = 'kernel SVM (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
-
 
